1c74f64a2e86049f203bf7db1764784cab43a924
hyunwoo <hyunwoo2546@naver.com>
2023-07-04 15:06:51 +0900

    Spring Data JPA

--- src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -4,8 +4,14 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -28,6 +34,93 @@ public class BoardRepositoryTests {
             log.info("BNO : " + result.getBno());
 
         });
+    }
+
+    @Test
+    public void testSelect() {
+        Long bno = 100L;
+
+        Optional<Board> result = boardRepository.findById(bno);
+
+        Board board = result.orElseThrow();
+
+        log.info(board);
+    }
+
+    @Test
+    public void testUpdate() {
+        Long bno = 100L;
+
+        Optional<Board> result = boardRepository.findById(bno);
+
+        Board board = result.orElseThrow();
+
+        board.change("update..title 100","update content 100");
+
+        boardRepository.save(board);
+    }
+
+    @Test
+    public void testDelete() {
+        Long bno = 100L;
+
+        boardRepository.deleteById(bno);
+
+    }
+
+    @Test
+    public void testPaging() {
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+
+        log.info("total cnt : " + result.getTotalPages());
+        log.info("total pages : " + result.getTotalPages());
+        log.info("page number : " + result.getNumber());
+        log.info("page size : " + result.getSize());
+
+        List<Board> todoList = result.getContent();
+
+        todoList.forEach(board -> log.info(board));
+        
+    }
+
+    @Test
+    public void testSearch1() {
+        Pageable pageable = PageRequest.of(1,10, Sort.by("bno").descending());
+
+        boardRepository.search1(pageable);
+    }
+
+    @Test
+    public void testSearchAll() {
+        String[] types = {"t","c","w"};
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0,10,Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.searchAll(types,keyword,pageable);
+    }
+
+    @Test
+    public void testSearchAll2() {
+        String[] types = {"t","c","w"};
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0,10,Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.searchAll(types,keyword,pageable);
+
+        log.info(result.getTotalPages());;
+        log.info(result.getSize());
+        log.info(result.getNumber());
+        log.info(result.hasPrevious() + ":" + result.hasNext());
+
+        result.getContent().forEach(board -> log.info(board));
+
+
     }
 
 }
