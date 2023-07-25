488449b2d13ed466dd50d39c21b29b4ec4bb78b3
hyunwoo <hyunwoo2546@naver.com>
2023-07-26 04:36:11 +0900

    v2.6 Reply 서버 처리 및 테스트 완료

---
+++ src/main/java/org/zerock/b01/domain/Reply.java
@@ -0,0 +1,31 @@
+package org.zerock.b01.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+//인덱스 태우기
+@Table(name = "Reply", indexes = {
+        @Index(name = "idx_reply_board_bno", columnList = "board_bno")
+})
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Reply extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    //@ManyToOne 다대일
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Board board;
+
+    private String replyText;
+
+    private String replyer;
+
+}
