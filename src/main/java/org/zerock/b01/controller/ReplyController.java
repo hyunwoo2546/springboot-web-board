fc43f75ec0da70dbd7d14185be6d91eeab8d22c5
hyunwoo <hyunwoo2546@naver.com>
2023-08-16 20:49:42 +0900

    v3.0 댓글 로직 구현

--- src/main/java/org/zerock/b01/controller/ReplyController.java
+++ src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -7,10 +7,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.dto.ReplyDTO;
 import org.zerock.b01.service.ReplyService;
 
@@ -25,27 +24,6 @@ import java.util.Map;
 public class ReplyController {
 
     private final ReplyService replyService;
-
-    //ApiOperation는 Swagger UI에서 해당 기능 설명
-/*    @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")
-    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String,Long> register(@Valid @RequestBody ReplyDTO replyDTO,
-                                     BindingResult bindingResult) throws BindException {
-
-        log.info(replyDTO);
-
-        if(bindingResult.hasErrors()) {
-            throw new BindException(bindingResult);
-        }
-
-        Map<String,Long> resultMap = new HashMap<>();
-
-        resultMap.put("rno" , 111l);
-
-        return resultMap;
-
-    }*/
-
     @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Map<String,Long> register(
@@ -65,6 +43,55 @@ public class ReplyController {
         resultMap.put("rno",rno);
 
         return resultMap;
+    }
+
+    @ApiOperation(value = "Replies of Board", notes = "GET 방식으로 특정 게시물의 댓글 목록")
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno, PageRequestDTO pageRequestDTO) {
+
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno,pageRequestDTO);
+
+        return responseDTO;
+    }
+
+    @ApiOperation(value = "Read Reply", notes = "GET 방식으로 특정 댓글 조회")
+    @GetMapping("/{rno}")
+    public ReplyDTO getReplyDTO(@PathVariable("rno") Long rno) {
+
+        ReplyDTO replyDTO = replyService.read(rno);
+
+        return replyDTO;
+
+    }
+
+    @ApiOperation(value = "Delete Reply", notes = "DELETE 방식으로 댓글 삭제")
+    @DeleteMapping("/{rno}")
+    public Map<String, Long> remove(@PathVariable("rno") Long rno) {
+
+        replyService.remove(rno);
+
+        Map<String, Long> resultMap = new HashMap<>();
+
+        resultMap.put("rno", rno);
+
+        return resultMap;
+
+    }
+
+    @ApiOperation(value = "Modify Reply", notes = "PUT 방식으로 특정 댓글 수정")
+    @PutMapping(value = "/{rno}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Long> modify(@PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO) {
+
+        replyDTO.setRno(rno); // 번호 일치시킴
+
+        replyService.modify(replyDTO);
+
+        Map<String, Long> resultMap = new HashMap<>();
+
+        resultMap.put("rno", rno);
+
+        return resultMap;
+
     }
 
 }
