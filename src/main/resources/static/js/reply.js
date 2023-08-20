985b54a2f63f312a800cc2d64fc80499eb29a6e3
hyunwoo <hyunwoo2546@naver.com>
2023-08-21 00:32:44 +0900

    v3.3 댓글 구현 (Axios)

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -20,15 +20,16 @@ async function addReply(replyObj) {
 }
 
 async function getReply(rno) {
-
     const response = await axios.get(`/replies/${rno}`)
-
     return response.data
-
 }
 
 async function modifyReply(replyObj) {
-
     const response = await axios.put(`/replies/${replyObj.rno}`, replyObj)
     return response.data
+}
+
+async function removeReply(rno) {
+    const respone = await axios.delete(`/replies/${rno}`)
+    return respone.data
 }
