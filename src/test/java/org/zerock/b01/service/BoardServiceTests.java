1275fe60fc546bf0fcce53f38c422df3345ad41d
hyunwoo <hyunwoo2546@naver.com>
2023-09-17 20:40:22 +0900

    v4.0 첨부파일 썸네일 작업

--- src/test/java/org/zerock/b01/service/BoardServiceTests.java
+++ src/test/java/org/zerock/b01/service/BoardServiceTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.b01.dto.*;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
