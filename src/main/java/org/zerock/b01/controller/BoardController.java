e2a87362b28eca274f159f102bf66684f6b27915
hyunwoo <hyunwoo2546@naver.com>
2023-09-25 22:46:30 +0900

    v4.5 첨부파일 파라미터 관리 및 remove 메소드 수정

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -3,6 +3,9 @@ package org.zerock.b01.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,12 +17,18 @@ import org.zerock.b01.dto.*;
 import org.zerock.b01.service.BoardService;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
+
+    @Value("${org.zerock.upload.path}")
+    private String uploadPath;
 
     private final BoardService service;
 
@@ -102,13 +111,49 @@ public class BoardController {
     }
 
     @PostMapping("/remove")
-    public String remove(Long bno) {
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
 
-        log.info("remove post : " + bno);
+        Long bno  = boardDTO.getBno();
+        log.info("remove post.. " + bno);
 
         service.remove(bno);
 
+        //게시물이 삭제되었다면 첨부 파일 삭제
+        log.info(boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+        if(fileNames != null && fileNames.size() > 0){
+            removeFiles(fileNames);
+        }
+
+        redirectAttributes.addFlashAttribute("result", "removed");
+
         return "redirect:/board/list";
+
+    }
+
+    public void removeFiles(List<String> files){
+
+        for (String fileName:files) {
+
+            Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+            String resourceName = resource.getFilename();
+
+
+            try {
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+                resource.getFile().delete();
+
+                //섬네일이 존재한다면
+                if (contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+                    thumbnailFile.delete();
+                }
+
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+
+        }//end for
     }
 
 }
